1f686bb3972258d2a120237628abadc84a27d041
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-10-15 10:31:58 +0530

    vault backup: 2024-10-15 10:31:58
    Affected files:
    LICENSE
    public/avatar.jpeg
    public/manifest.webmanifest
    src/components/Avatar.astro
    src/pages/index.astro
    src/site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,11 +2,11 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Shriram Balaji",
+	author: "Abhishek Tripathi",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Shriram Balaji's Blog",
+	title: "Abhishek Tripathi's Blog",
 	// Meta property used as the default description meta property
-	description: "Thoughts, writings and musings of Shriram Balaji",
+	description: "Thoughts, writings of Abhishek Tripathi",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-US",
 	// Meta property, found in src/components/BaseHead.astro L:42
@@ -21,7 +21,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 
-	handle: "@shrirambalaji",
+	handle: "@twistin456",
 };
 
 // Used to generate links in both the Header & Footer.
