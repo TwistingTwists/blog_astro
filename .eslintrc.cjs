be8dd7d5003bbf3007a7f825868bbaf21740e23f
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-12-05 10:03:43 +0530

    feat: setup for deployment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,50 @@
-/** @type {import("@types/eslint").Linter.Config} */
-module.exports = {
-	ignorePatterns: ["node_modules", "dist"],
-	root: true,
-	env: {
-		node: true,
-	},
-	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint", "prettier"],
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:prettier/recommended",
-		"plugin:astro/recommended",
-		"plugin:astro/jsx-a11y-recommended",
-	],
-	rules: {
-		"@typescript-eslint/no-var-requires": "warn",
-		"@typescript-eslint/no-unused-vars": [
-			"warn",
-			{ varsIgnorePattern: "Props", ignoreRestSiblings: true },
-		],
-	},
-	overrides: [
-		{
-			files: ["*.astro"],
-			parser: "astro-eslint-parser",
-			parserOptions: {
-				parser: "@typescript-eslint/parser",
-				extraFileExtensions: [".astro"],
-			},
-			rules: {
-				"prettier/prettier": "off",
-			},
-		},
-	],
-};
+// /** @type {import("@types/eslint").Linter.Config} */
+// module.exports = {
+// 	ignorePatterns: ["node_modules", "dist"],
+// 	root: true,
+// 	env: {
+// 		node: true,
+// 	},
+// 	// parser: "@typescript-eslint/parser",
+// 	plugins: ["unused-imports"],
+// 	// plugins: ["@typescript-eslint", "unused-imports"],
+// 	extends: [
+// 		"eslint:recommended",
+// 		"plugin:oxlint/recommended",
+// 		"plugin:@typescript-eslint/recommended",
+// 		"plugin:prettier/recommended",
+// 		"plugin:astro/recommended",
+// 		"plugin:astro/jsx-a11y-recommended",
+// 	],
+// 	rules: {
+// 		// "@typescript-eslint/no-var-requires": "warn",
+// 		// "@typescript-eslint/no-unused-vars": [
+// 		// 	"warn",
+// 		// 	{ varsIgnorePattern: "Props", ignoreRestSiblings: true },
+// 		// ],
+// 		"no-unused-vars": "off",
+// 		"unused-imports/no-unused-vars": [
+// 			"warn",
+// 			{
+// 				"vars": "all",
+// 				"varsIgnorePattern": "^_",
+// 				"args": "after-used",
+// 				"argsIgnorePattern": "^_"
+// 			}
+// 		]
+
+// 	},
+// 	overrides: [
+// 		{
+// 			files: ["*.astro"],
+// 			parser: "astro-eslint-parser",
+// 			parserOptions: {
+// 				parser: "@typescript-eslint/parser",
+// 				extraFileExtensions: [".astro"],
+// 			},
+// 			rules: {
+// 				"prettier/prettier": "off",
+// 			},
+// 		},
+// 	],
+// };
